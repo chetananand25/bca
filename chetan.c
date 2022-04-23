1ad448b9e26ab1b6f3b4940cb4d4d0ce7783bebb
chetananand25 <104248360+chetananand25@users.noreply.github.com>
2022-04-23 13:56:57 +0530

    Add files via upload

---
+++ chetan.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("hello worl");
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}
---
+++ emi calculator.js
@@ -0,0 +1,6 @@
+function Ecalculater(){
+    la=document.getElementsById("lamt").value;
+    lt=document.getElementsById("ltim").value;
+    li=document.getElementsById("lint").value;
+    document.getElementById("box").innerHTML="OUTPUT"+la;
+}
